d40b0515d651682ca1f3fcfb71dde8211b081627
alwaysJOne <alwaysjone@naver.com>
2025-11-12 15:52:14 +0900

    .

--- 05_Spring/spring/src/main/java/com/kh/spring/controller/HomeController.java
+++ 05_Spring/spring/src/main/java/com/kh/spring/controller/HomeController.java
@@ -2,12 +2,26 @@ package com.kh.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String home(){
+    public String home(@RequestParam(value = "cpage", defaultValue = "1") int currentPage){
+        //공중화장실 목록을 받아서 페이지에 전달
+        //http://openapi.seoul.go.kr:8088/인증키/응답형식/mgisToiletPoi/시작인덱스/끝인덱스/
+        //http://openapi.seoul.go.kr:8088/48764a4169776c6431313078426c7341/json/mgisToiletPoi/6/15/
+
+        //PageInfo를 사용해서 페이징 처리
+        int itemLimit = 10; //한페이지에 보여줄 화장실 갯수
+        int pageLimit = 5; //페이징 버튼 갯수
+
+        int startIndex = (currentPage - 1) * itemLimit + 1;
+        int endIndex = startIndex + itemLimit - 1;
+
+
+
         return "index";
     }
 }
