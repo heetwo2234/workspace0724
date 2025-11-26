7725622f9349b35cfa94c2f354aafd7ee66f99b0
alwaysJOne <alwaysjone@naver.com>
2025-11-26 14:47:42 +0900

    .

--- 06_React/react-project/src/App.jsx
+++ 06_React/react-project/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UseStateTest from './components/useState/UseStateTest'
+import Signup from './components/useState/Signup'
+import LandingPage from './components/useState/LandingPage'
+import UseRefTest from './components/useRef/UseRefTest'
+import UseRefScroll from './components/useRef/UseRefScroll'
+import UseMemoTest from './components/useMemo/UseMemoTest'
+import UseCallbackTest from './components/useCallback/UseCallbackTest'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      {/* <UseStateTest /> */}
+      {/* <Signup /> */}
+      {/* <LandingPage /> */}
+      {/* <UseRefTest /> */}
+      {/* <UseRefScroll /> */}
+      {/* <UseMemoTest /> */}
+      <UseCallbackTest />
     </>
   )
 }
